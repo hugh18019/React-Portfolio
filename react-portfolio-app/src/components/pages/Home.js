7784313c2417e9d8ae51e6a65fe55f93dbe0d3a0
Huiran Lin <hugh80082@gmail.com>
2021-08-25 20:51:43 -0700

    Add content to home page

--- react-portfolio-app/src/components/pages/Home.js
+++ react-portfolio-app/src/components/pages/Home.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '../style.css';
 
 export default function Home() {
   return (
     <div>
-      <section>This is Huiran's Porfolio Website.</section>
+      <div className='d-flex justify-content-center align-item-center'>
+        <section>This is Huiran's Porfolio Website.</section>
+      </div>
+
       <footer>
         <h4>Contact Me</h4>
         <p>
           <p id='email'>hugh80082@gmail.com</p>
-          <a href='https://www.linkedin.com/in/huiran-lin-0999b419b/'>
+          <a
+            className='mr-3'
+            href='https://www.linkedin.com/in/huiran-lin-0999b419b/'
+          >
             LinkedIn
           </a>
           <a href='https://github.com/hugh18019'>GitHub</a>
